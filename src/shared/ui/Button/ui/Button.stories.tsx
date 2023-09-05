a02ca91fbf07dc9e380202f8aa1fc92bbf6ad26e
STEIN <doruchenko.m.i@gmail.com>
2023-09-05 18:49:56 +0300

    added modal

--- src/shared/ui/Button/ui/Button.stories.tsx
+++ src/shared/ui/Button/ui/Button.stories.tsx
@@ -25,6 +25,12 @@ Primary.args = {
 export const Clear = Template.bind({});
 Clear.args = {
   children: 'Text',
+  theme: ButtonTheme.CLEAR_INVERTED,
+};
+
+export const ClearInverted = Template.bind({});
+ClearInverted.args = {
+  children: 'Text',
   theme: ButtonTheme.CLEAR,
 };
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonTheme } from 'shared/ui/Button';
+import { Modal } from 'shared/ui/Modal';
 import classes from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -9,9 +12,27 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
 
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev: any) => !prev);
+  }, []);
+
   return (
     <div className={classNames(classes.Navbar, {}, [className])}>
-      <div className={classes.links}>/</div>
+      <Button
+        theme={ButtonTheme.CLEAR_INVERTED}
+        className={classes.links}
+        onClick={onToggleModal}
+      >
+        {t('Sign in')}
+      </Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        {/* eslint-disable-next-line */}
+        {t(
+          'Lorem ipsum dolor sit amet consectetur, adipisicing elit. At, minima.'
+        )}
+      </Modal>
     </div>
   );
 };
